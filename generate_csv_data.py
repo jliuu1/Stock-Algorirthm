8f01fc84ce3a5aed1cf358b916308e48f9935e22
Colin Wang <colwang@umich.edu>
2020-12-29 21:46:47 -0500

    changed buffer per year

--- generate_csv_data.py
+++ generate_csv_data.py
@@ -9,10 +9,11 @@ start_time = time.time()
 calc = Calculator('stock_tickers.csv')
 
 year = 2000
-segment = 5
+year_segment = 5
 month = '01'
 day = '02'                                                  # 01 is New Year's Day, market's aren't open
-one_thousand_days = datetime.timedelta(days = 1000)         
+buffer_per_year = 200
+days_buffer = datetime.timedelta(days = buffer_per_year * year_segment)         
 
 with open("training_data_2.csv", "w") as data_file:
 	
@@ -23,12 +24,12 @@ with open("training_data_2.csv", "w") as data_file:
         ticker = calc.ticker_names[i]
         first_listing = calc.ticker_starts[ticker]
 
-        for i in range(2020 - year - segment):
+        for i in range(2020 - year - year_segment):
             start_date = str(year + i) + "-" + month + "-" + day
             end_date = str(year + i + 5) + "-" + month + "-" + day
 
             # no point for the data if more than 1000 entries are zeros
-            if ((calc.get_date(start_date) + one_thousand_days) > first_listing):              
+            if ((calc.get_date(start_date) + days_buffer) > first_listing):              
                 calc.calculate_ticker_data_for_timeframe(writer, ticker, start_date, end_date)
 
 
